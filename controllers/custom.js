cd8aca76c074b3c1e1af6e8362c8c7220a97d129
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-12 15:18:17 -0500

    wip: adds custom/index

--- controllers/custom.js
+++ controllers/custom.js
@@ -2,7 +2,10 @@ const express = require('express');
 const customRouter = express.Router();
 const Custom = require('../models/custom');
 
-
+//Index Route
+customRouter.get('/', (req, res) => {
+    res.render('custom/index.ejs')
+});
 
 
 
