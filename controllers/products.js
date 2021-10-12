e75e79ab397566667c8ae85c1e1be3c08f8e9440
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-11 19:07:22 -0500

    wip: adds create route

--- controllers/products.js
+++ controllers/products.js
@@ -2,10 +2,17 @@ const express = require('express');
 const productRouter = express.Router();
 const Product = require('../models/product');
 
+// //New Route
+// productRouter.get('')
 
 
+//Create Route
+productRouter.post('/', (req, res) => {
+    Product.create(req.body, (error, createdProduct) => {
 
-
+        res.send(createdProduct)
+    });
+});
 
 
 
