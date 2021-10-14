ef9ea469f8378b69840586c81847b1f21c1086c7
“coreyalvarez” <coreyalvarez1989@gmail.com>
2021-10-14 13:53:37 -0500

    add redirect

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 
+app.get('/', (req, res) => res.redirect('/home'));
 
 app.use('/custom', customController);
 app.use('/products', productsController);
